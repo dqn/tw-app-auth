c86aef0e9402007ab32da7a175ad2380cee72270
dqn <suyarizzz@gmail.com>
2020-04-30 18:04:14 +0900

    feat: load config

--- go.mod
+++ go.mod
@@ -4,5 +4,5 @@ go 1.14
 
 require (
 	github.com/labstack/echo/v4 v4.1.16
-	gopkg.in/yaml.v2 v2.2.8 // indirect
+	gopkg.in/yaml.v2 v2.2.8
 )
--- main.go
+++ main.go
@@ -1,22 +1,81 @@
 package main
 
 import (
+	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
+	"net/url"
+	"os"
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
+	"gopkg.in/yaml.v2"
 )
 
+type config struct {
+	ConsumerKey    string `yaml:"consumer_key"`
+	ConsumerSecret string `yaml:"consumer_secret"`
+	CallbackURL    string `yaml:"callback_url"`
+}
+
+func loadConfig(path string) (*config, error) {
+	f, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+
+	b, err := ioutil.ReadAll(f)
+	if err != nil {
+		return nil, err
+	}
+
+	var c config
+	if err = yaml.Unmarshal(b, &c); err != nil {
+		return nil, err
+	}
+
+	return &c, nil
+}
+
+func getPort(u *url.URL) (string, error) {
+	if u.Port() != "" {
+		return u.Port(), nil
+	}
+	switch u.Scheme {
+	case "http":
+		return "80", nil
+	case "https":
+		return "443", nil
+	default:
+		return "", fmt.Errorf("invalid scheme %s", u.Scheme)
+	}
+}
+
 func run() error {
+	c, err := loadConfig("./config.yml")
+	if err != nil {
+		return err
+	}
+
+	u, err := url.Parse(c.CallbackURL)
+	if err != nil {
+		return err
+	}
+
 	e := echo.New()
 
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
 
-	e.GET("/", hello)
+	e.GET(u.Path, hello)
 
-	e.Logger.Fatal(e.Start(":3000"))
+	port, err := getPort(u)
+	if err != nil {
+		return err
+	}
+
+	e.Logger.Fatal(e.Start(":" + port))
 
 	return nil
 }
